be34c2e6b776e9972e67a369a1384f4a798ca216
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-27 15:53:43 +0200

    Import from active_record-associated_object

--- test/active_job/test_performs.rb
+++ test/active_job/test_performs.rb
@@ -2,12 +2,23 @@
 
 require "test_helper"
 
-class ActiveJob::TestPerforms < Minitest::Test
-  def test_that_it_has_a_version_number
+class ActiveJob::TestPerforms < ActiveSupport::TestCase
+  include ActiveJob::TestHelper
+
+  setup do
+    Post::Publisher.performed = false
+    @publisher = Post::Publisher.new(1)
+  end
+
+  test "version number" do
     refute_nil ::ActiveJob::Performs::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  test "active job integration" do
+    assert_performed_with job: Post::Publisher::PublishJob, args: [ @publisher ], queue: "important" do
+      @publisher.publish_later
+    end
+
+    assert Post::Publisher.performed
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,28 @@ require "active_job"
 require "active_job/performs"
 
 require "minitest/autorun"
+
+class ApplicationJob < ActiveJob::Base; end
+
+GlobalID.app = :performs
+GlobalID::Locator.use(:performs) { Post::Publisher.new(_1.model_id.to_i) }
+
+module Post; end
+
+class Base < Struct.new(:id)
+  include GlobalID::Identification
+  singleton_class.alias_method :find, :new
+end
+
+class Post::Publisher < Base
+  extend ActiveJob::Performs
+
+  singleton_class.attr_accessor :performed
+
+  performs queue_as: :not_really_important
+  performs :publish, queue_as: :important, discard_on: ActiveJob::DeserializationError
+
+  def publish
+    self.class.performed = true
+  end
+end
